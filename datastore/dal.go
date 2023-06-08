848332efdaaf24573db2db529c4820b935f9baca
mukul vashisht <mukulvashisht1026@gmail.com>
2023-06-09 01:54:46 +0530

    added generic dal

---
+++ datastore/dal.go
@@ -0,0 +1,50 @@
+package datastore
+
+import "database/sql"
+
+type GenericDatabaseLayer interface {
+	Connect() error
+	Disconnect() error
+	Query(query string, args... interface{}) (*sql.Rows, error)
+	Insert(table string, values... interface{}) (int64, error)
+}
+
+
+type MysqlDatabaseLayer struct {
+	Db *sql.DB
+}
+
+func NewMysqlDatabaseLayer(dataSourceName string) (*MysqlDatabaseLayer, error) {
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return &MysqlDatabaseLayer{Db: db}, nil
+}
+
+func (m *MysqlDatabaseLayer) Connect() error {
+	return m.Db.Ping()
+}
+
+func (m *MysqlDatabaseLayer) Disconnect() error {
+	return m.Db.Close()
+}
+
+func (m *MysqlDatabaseLayer) Query(query string, args... interface{}) (*sql.Rows, error) {
+	return m.Db.Query(query, args...)
+}
+
+func (m *MysqlDatabaseLayer) Insert(table string, values... interface{}) (int64, error) {
+	stmt, err := m.Db.Prepare("INSERT INTO " + table + " VALUES (?)")
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	
+	result, err := stmt.Exec(values...)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.RowsAffected()
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/net v0.10.0 // indirect
 )
